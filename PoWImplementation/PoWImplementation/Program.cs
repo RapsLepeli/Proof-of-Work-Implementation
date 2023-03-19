ba6bc59120cf99aed01eb91d7e9216b91f612ee2
Raps Lepeli <rapelanglepeli2000@gmail.com>
2023-03-19 15:27:12 +0200

    Let's begin.

---
+++ PoWImplementation/PoWImplementation/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PoWImplementation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            Console.Write("\nPress any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
